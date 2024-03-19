44526418880062d56367e50e62c21c630199b56c
Tom Vance <tom@loyaltylion.com>
2024-03-19 19:32:37 +0000

    Include iterm config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,6 +24,9 @@ return {
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'D', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '.', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', 'F', function()
+        vim.lsp.buf.format { async = true }
+      end, {})
     end
   }
 }
